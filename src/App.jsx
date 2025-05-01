cc616f79a7fbc0d0db9fc312f4a98b7563d6a427
Dushy-uki <dushyd81@gmail.com>
2025-05-01 12:53:01 +0530

    add path

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
   return (
     <Router> 
       <Routes>
+      <Route path="/" element={<User-Dashboard-Blog-Page />} />
         <Route path="/" element={<Login />} />
         <Route path="/user-dashboard" element={<User />} />
         <Route path="/admin-dashboard" element={<Admin />} />
